96f2d521a4b131185bd1ae5834e87eeaa1e1a8f1
marrullas <marrullas@gmail.com>
2016-04-04 19:53:15 +0000

    ajutes de direccionamiento despues del login

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -34,6 +34,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        //return view('home');
+        return redirect()->action('ProfileController@index');
     }
 }
