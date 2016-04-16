449cb14a2787b0740776000de9889e1aee335f0b
marrullas <marrullas@gmail.com>
2016-04-16 13:57:44 +0000

    mensaje en las imagenes

--- resources/views/profile/education/partials/fields.blade.php
+++ resources/views/profile/education/partials/fields.blade.php
@@ -134,7 +134,7 @@
         </div>
     </div>
     <div class="form-group">
-        <label for="inputName" class="control-label">Evidencia/Diploma</label>
+        <label for="inputName" class="control-label">Evidencia/Diploma (Solo imagenes, Maximo 1000kb)</label>
         @if(isset($educa))
             @if($educa['archivo'])
                 <div>
